d483eaf27314123955bedf9f709318c5a3de2cda
brightiron <brightiron@protonmail.com>
2022-08-25 21:13:34 -0500

    landing page layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,50 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const math = require('remark-math');
-const katex = require('rehype-katex');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'OlympusDAO', // Usually your GitHub org/user name.
-  projectName: 'olympus-docs', // Usually your repo name.
+  organizationName: "OlympusDAO", // Usually your GitHub org/user name.
+  projectName: "olympus-docs", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/main', // defaults to docs, but `main` maintains the gitbook paths
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/main", // defaults to docs, but `main` maintains the gitbook paths
+          sidebarPath: require.resolve("./sidebars.js"),
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/OlympusDAO/olympus-docs/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/OlympusDAO/olympus-docs/tree/main/packages/create-docusaurus/templates/shared/",
           remarkPlugins: [math],
           rehypePlugins: [katex],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -52,77 +52,83 @@ const config = {
   // config for KaTex plugin: https://docusaurus.io/docs/markdown-features/math-equations
   stylesheets: [
     {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
       integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
+        "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
     },
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: "light",
+        disableSwitch: false,
+        respectPrefersColorScheme: false,
+      },
       navbar: {
-        title: 'My Site',
+        title: "",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "Olympus Docs",
+          src: "img/logo.svg",
+          srcDark: "img/logo_dark.svg",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'basics/readme',
-            position: 'left',
-            label: 'Protocol Docs',
+            type: "doc",
+            docId: "basics/readme",
+            position: "left",
+            label: "Protocol Docs",
           },
           {
-            href: 'https://github.com/OlympusDAO/olympus-docs',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/OlympusDAO/olympus-docs",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Protocol Docs',
-                to: '/main/basics/readme',
+                label: "Protocol Docs",
+                to: "/main/basics/readme",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Discord',
-                href: 'https://discord.com/invite/olympusdao',
+                label: "Discord",
+                href: "https://discord-invite.olympusdao.finance/",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/OlympusDAO',
+                label: "Twitter",
+                href: "https://twitter.com/OlympusDAO",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: 'https://www.olympusdao.finance/blog',
+                label: "Blog",
+                to: "https://www.olympusdao.finance/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/OlympusDAO',
+                label: "GitHub",
+                href: "https://github.com/OlympusDAO",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Olympus DAO, Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Olympus DAO`,
       },
       prism: {
         theme: lightCodeTheme,
