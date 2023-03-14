0785d37f2c1cef39aca3e009de7c96a74b50579a
cptnanon <93794078+cptnanon@users.noreply.github.com>
2023-03-14 11:02:55 +0100

    added gtm container id (#91)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,9 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        googleTagManager: {
+          containerId: 'GTM-NJSWZX5'
+        }
       }),
     ],
   ],
